131f595572a9167d497b26514c063f263eed2040
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2019-03-03 03:23:44 +0530

    Add raw corpus extract function

--- data.py
+++ data.py
@@ -5,5 +5,13 @@ def save_to_npy(filename):
     print("Saving to npy file: {}".format("nepal.npy"))
     np.save("nepal.npy", file)
 
+def extract_corpus(filename):
+    print("Reading dataset, filename:{}".format(filename))
+    file = np.loadtxt(filename, dtype=str, delimiter='\t',encoding=None,comments=chr(0))
+    corpus = file[:,[1,3]]
+    print("Saving corpus, filename:{}".format("raw_corpus.txt"))
+    np.savetxt("raw_corpus.txt",corpus, fmt="%s", comments=chr(0), delimiter='\t')
+
 if __name__ == "__main__":
-    save_to_npy("nepal_TMCS_JOURNAL_THREE_10032017.txt")
+    extract_corpus("nepal_TMCS_JOURNAL_THREE_10032017.txt")    
+    #save_to_npy("nepal_TMCS_JOURNAL_THREE_10032017.txt")
