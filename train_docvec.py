769ed4b96ae41220577a6916aaf729c57a1b5253
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2019-04-07 07:20:20 +0530

    docvec for doc embedding. [Useful in graph construction]

---
+++ train_docvec.py
@@ -0,0 +1,87 @@
+from gensim.models.doc2vec import (
+	Doc2Vec,
+	TaggedDocument,
+)
+import multiprocessing
+from gensim.models.callbacks import CallbackAny2Vec
+from datetime import datetime
+import os
+import gensim
+import pandas as pd
+import re
+
+dataset = "dataset/nepal/"
+
+train = dataset + "2015_Nepal_Earthquake_train.tsv"
+train_tweets = pd.read_csv(train, sep="\t", encoding="latin")
+
+# unlabelled = dataset + "2015_Nepal_Earthquake_unlabelled.tsv"
+
+dev = dataset + "2015_Nepal_Earthquake_dev.tsv"
+dev_tweets = pd.read_csv(dev, sep="\t", encoding="latin")
+
+tweets = dict()
+tag_to_tweet = dict()
+tag_to_id = dict()
+
+def parsing(tweet):
+	tweet = tweet.lower() #lowercase
+	tweet = re.sub(r'http\S+', "", tweet) #Remove url
+	tweet = re.sub("\d", "", tweet)
+	tweet = re.sub("[^a-zA-Z\.\@\#\s]", "", tweet)
+	words = re.split(" ", tweet)
+	new_words = []
+	for word in words:
+		if len(word) < 2:
+			pass
+		elif word[0] == '@':
+			pass
+		elif word[0] == '#':
+			pass
+		else:
+			new_words.append(word)
+	tweet = " ".join(new_words)
+	tweet = re.sub("\s\s+"," ",tweet)
+	return tweet
+
+for tweet_collection in [train_tweets, dev_tweets]:
+	for index, row in tweet_collection.iterrows():
+		tweets[row[0]] = parsing(row[1])
+
+class EpochLogger(CallbackAny2Vec):
+	'''Callback to log information about training'''  
+	def __init__(self, path):
+		self.epoch = 0
+		self.path = path
+		self.start_time = datetime.now()
+	def on_epoch_begin(self,  model):
+		pass                                 
+	def on_epoch_end(self,  model):
+		self.epoch += 1
+		print("Epoch #{} end, time elapsed: {}".format(self.epoch, datetime.now()-self.start_time))
+
+class corpus(object):
+	"""docstring for corpus"""
+	def __init__(self, file):
+		super(corpus, self).__init__()
+		self.dict = file
+	def __iter__(self):
+		for i, ID in enumerate(self.dict.keys()):
+			tag_to_tweet[i] = tweets[ID]
+			tag_to_id[i] = ID
+			yield TaggedDocument(gensim.utils.simple_preprocess(tweets[ID]), tags=[i])
+
+epoch_logger = EpochLogger("tweet_doc2vec")
+print("model initialised")
+
+model = Doc2Vec(seed=0,workers = multiprocessing.cpu_count(), window=5,  callbacks=[epoch_logger],min_count=1, vector_size=300, epoch=10)
+
+print("Corpus instance initialised")
+cor = corpus(tweets)
+
+print("build vocab")
+model.build_vocab(cor)
+
+print("train")
+model.train(cor, total_examples=model.corpus_count, epochs=model.epochs)
+model.save("tweet_doc2vec.model")
